77ef5fc61f04c0a956647747c05cebb1142af4bc
Ajaya Panda <ajayakumarpanda95@gmail.com>
2021-02-24 15:32:55 +0530

    lis completed

--- src/gfg/dp/LongestIncreasingSubsequence.java
+++ src/gfg/dp/LongestIncreasingSubsequence.java
@@ -3,19 +3,36 @@ package gfg.dp;
 public class LongestIncreasingSubsequence {
 
 	public static void main(String[] args) {
-		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 }; 
+		int arr[] = {50, 3, 10, 7, 40, 80}; 
 		int n = arr.length; 
+		int temp = Integer.MIN_VALUE;
         System.out.println("Length of lis is "
                            + lis(arr, n) + "\n"); 
 	}
 
 	private static int lis(int[] arr, int n) {
-		if(n == 1)
-			return 1;
-		for (int i = 0; i < arr.length; i++) {
-			lis(arr, i);
+		int output[] = new int[n];
+		output[0] = 1;
+		
+		for (int i = 1; i < arr.length; i++) {
+			for(int j = i-1; j >= 0; j--) {
+				if(arr[i] > arr[j]) {
+					int possibleAns = output[j] + 1;
+					if(possibleAns > output[j]) {
+						output[j] = possibleAns;
+					}
+				}
+			}
 		}
-		return 0;
+		int best = 0;
+		for(int val: output) {
+			if(best < val) {
+				best = val;
+			}
+		}
+		
+		
+		return best;
 	}
 
 }
